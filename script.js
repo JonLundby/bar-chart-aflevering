0790bd6cfadcd87066f1f4745b3710756456be33
Jon <lundbyjon@hotmail.com>
2023-04-11 22:30:25 +0200

    copy paste html js and css from another project folder

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// const tempArray = [10, 20, 30, 15, 8, 18, 34, 22, 6, 17];
+// const myHeight = 50;
+
+const barArray = [];
+let barHeight = getNumberOfCustomers();
+const allBars = document.querySelectorAll("#bar");
+
+window.addEventListener("load", startApp);
+
+function startApp() {
+  setInterval(updateNumbersOfCustomers, 1000);
+}
+
+function updateNumbersOfCustomers() {
+  barArray.push(barHeight);
+
+  updateBarHeight();
+  highLoadColor();
+
+  if (barArray.length == 40) {
+    barArray.shift();
+  }
+
+  console.log(barArray);
+
+  barHeight = getNumberOfCustomers();
+}
+
+function getNumberOfCustomers() {
+  return Math.floor(Math.random() * 32);
+}
+
+function updateBarHeight() {
+  for (let i = 0; i < allBars.length; i++) {
+    for (let j = 0; j < barArray.length; j++) {
+      allBars[j].style.height = `${barArray[j]}%`;
+    }
+  }
+}
+
+function highLoadColor() {
+  if (barHeight > 15) {
+    document.querySelector("#bar").style.color = "red";
+  }
+}
